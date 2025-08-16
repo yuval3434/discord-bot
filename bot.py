7cc4d9bb3c964a2636aee8effbf8c92d91eb0138
yuval3434 <120325166+yuval3434@users.noreply.github.com>
2025-08-16 15:48:49 +0300

    music on voicechat added

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import random
 import discord
 from discord.ext import commands
 import json
+import yt_dlp
 
 with open("config.json", "r") as f:
     config = json.load(f)
@@ -45,12 +46,6 @@ async def flip(ctx):
 async def dice(ctx):
     await ctx.send(random.randint(1, 6))
 
-@commands.Cog.listener()
-async def on_member_join(self, member):
-    channel = discord.utils.get(member.guild.text_channels, name="general")
-    if channel:
-        await channel.send(f"Welcome {member.mention}!")
-
 @bot.command()
 async def userinfo(ctx, member: discord.Member = None):
     if member is None:
@@ -64,5 +59,40 @@ async def userinfo(ctx, member: discord.Member = None):
     embed.add_field(name="Roles", value=", ".join([role.name for role in member.roles if role.name != "@everyone"]), inline=False)
 
     await ctx.send(embed=embed)
+
+@bot.event
+async def on_member_join(member):
+    channel = discord.utils.get(member.guild.text_channels, name="general")
+    if channel:
+        await channel.send(f"Welcome {member.mention}!")
+
+@bot.command()
+async def play(ctx, *, query):
+    if not ctx.voice_client:
+        if ctx.author.voice:
+            await ctx.author.voice.channel.connect()
+        else:
+            return await ctx.send("❌ You need to be in a voice channel first!")
+
+    ydl_opts = {'format': 'bestaudio', 'noplaylist': True, 'quiet': True}
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(f"ytsearch:{query}", download=False)['entries'][0]
+        url = info['url']
+
+        ctx.voice_client.stop()
+        source = await discord.FFmpegOpusAudio.from_probe(url, method='fallback')
+        ctx.voice_client.play(source)
+
+        await ctx.send(f"🎶 Now playing: **{info['title']}**")
+
+    @bot.command()
+    async def pause(ctx):
+        if ctx.voice_client.is_playing():
+            ctx.voice_client.pause()
+
+    @bot.command()
+    async def resume(ctx):
+        if ctx.voice_client.is_paused():
+            ctx.voice_client.resume()
 
 bot.run(TOKEN)
